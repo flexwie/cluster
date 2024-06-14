2bc98228a73bfbea5f37bfdf30d2504da930de63
Felix Wieland <mail@felixwie.com>
2024-06-14 14:19:59 +0200

    fix: encrypted keys

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -4,5 +4,5 @@ cd "${scriptDir}/.." || exit 1
 
 export SOPS_AGE_RECIPIENTS=$(<public-age-keys.txt)
 exec 3<<< "$(cat $1)"
-sops --encrypt --input-type yaml --output-type yaml --age ${SOPS_AGE_RECIPIENTS} --encrypted-regex "^(user|password)$" /dev/fd/3
+sops --encrypt --input-type yaml --output-type yaml --age ${SOPS_AGE_RECIPIENTS} --encrypted-regex "^(user|password|token|secret)$" /dev/fd/3
 
